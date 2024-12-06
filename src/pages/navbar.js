785e468e4118c3bb1a1ffb24d6009b7cbe7fb8e2
SpaceShade <120098872+SpaceShade@users.noreply.github.com>
2024-12-06 14:06:00 +0700

    navbar tablet view

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -5,11 +5,12 @@ import user from '../assets/user.png'
 import bag from '../assets/shopping-bag.png' 
 import language from '../assets/language.png'
 import { useCart } from './cartContext'
-import { MobileView, BrowserView } from 'react-device-detect';
+import { MobileView, BrowserView, TabletView } from 'react-device-detect';
 function Navbar (){
     const {totalItems } = useCart();
   return (
-    <><BrowserView>
+    <>
+    <BrowserView>
       <div className="container">
         <div className="p-8">
           <div className="navbar fixed top-0 left-0 w-full bg-white shadow-md z-50 flex items-center ">
@@ -50,7 +51,8 @@ function Navbar (){
           </div>
         </div>
       </div>
-    </BrowserView><MobileView>
+    </BrowserView>
+    <MobileView>
         <div className="container">
           <div className="p-8">
             <div className="navbar fixed top-0 bg-white left-0 w-full z-50 flex items-center ">
@@ -82,7 +84,7 @@ function Navbar (){
                 </div>
               </div>
               <div className="absolute -z-50 bg-white top-11 left-0 w-full  shadow-md">
-                    <ul className="flex p-4 text-sm font-medium justify-between">
+                    <ul className="flex m-4 text-sm font-medium justify-between">
                       <li><a href="/" >ผู้หญิง</a></li>
                       <li><a href="/" >ผู้ชาย</a></li>
                       <li><a href="/" >เด็กอ่อน</a></li>
@@ -95,7 +97,49 @@ function Navbar (){
             </div>
           </div>
         </div>
-      </MobileView></>
+      </MobileView>
+      <TabletView>
+      <div className="container">
+        <div className="p-8">
+          <div className="navbar fixed top-2 left-0 w-full bg-white z-50 flex items-center ">
+            <img
+              src="https://th.bing.com/th/id/OIP.FPqaiQ5CFCAcFMvmnlxcDwHaE4?rs=1&pid=ImgDetMain"
+              className="w-16 h-auto mx-8 my-5"
+              alt="Logo" />
+            <ul className="flex space-x-6">
+              <li><a href="/" className="">ผู้หญิง</a></li>
+              <li><a href="/" className="">ผู้ชาย</a></li>
+              <li><a href="/" className="">เด็กอ่อน</a></li>
+              <li><a href="/" className="">เด็ก</a></li>
+              <li><a href="/" className="">Sport</a></li>
+              <li><a href="/" className="">Home</a></li>
+            </ul>
+            <div className="flex space-x-6 items-center ml-auto mr-8">
+              <a href="/"><img src={search} className="h-5 w-5" alt='search' /></a>
+              <a href="/"><img src={user} className="h-5 w-5" alt='user' /></a>
+              <a href="/"><img src={heart2} className="h-5 w-5" alt='heart2' /></a>
+              <div className='relative'>
+                <a href="./cart">
+                  <img src={bag} alt='bag' className="h-5 w-5" />
+                </a>
+                {totalItems > 0 && (
+                  <span className='absolute right-1.5 text-xs font-bold text-center'>
+                    {totalItems}
+                  </span>
+                )}
+              </div>
+              <div className="flex items-center space-x-1">
+                <img src={language} className="h-5 w-5" alt="Language Icon" />
+                <select className="bg-white">
+                  <option>TH</option>
+                  <option>ENG</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </TabletView></>
   )
 }
 
